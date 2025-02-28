ace654684d819479dbc641d30221e74fbf58df45
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-02-28 02:53:31 -0500

    feat: add task enums

---
+++ src/main/java/com/andorla/platform/todolist/Tasks/domain/model/valueobjects/PriorityType.java
@@ -0,0 +1,7 @@
+package com.andorla.platform.todolist.Tasks.domain.model.valueobjects;
+
+public enum PriorityType {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/andorla/platform/todolist/Tasks/domain/model/valueobjects/StatusType.java
@@ -0,0 +1,7 @@
+package com.andorla.platform.todolist.Tasks.domain.model.valueobjects;
+
+public enum StatusType {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
